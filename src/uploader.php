7846c59300edcfd0fc775c7ebf982ffd50117e45
david-perez <hallaplay835@gmail.com>
2016-08-01 19:36:49 +0200

    Refactoring - Better code structure. - Consistent code style.

--- src/uploader.php
+++ src/uploader.php
@@ -1,58 +1,67 @@
 <?php
 
-require __DIR__."/../vendor/autoload.php";
+require '../vendor/autoload.php';
 
 use Hunter\Hunter;
 
 loadConfig();
 
-function messageFormatter($number, $title, $id = NULL) {
-    $commit = sprintf($_ENV['COMMIT_MESSAGE_PATTERN'], $number, $title);
-    if ($id != NULL) $commit .= sprintf($_ENV['COMMIT_COMMENT_PATTERN'], $id);
-    return $commit;
-}
-
 $hunter = new Hunter();
 
-$ls = array();
-
-exec("ls ".$_ENV['PROBLEMS_DIR'], $ls);
+exec("ls $_ENV[PROBLEMS_DIR]", $ls);
 
 $problems = array();
-
 foreach ($ls as $file) {
-    if (preg_match($_ENV['FILE_REGEX_PATTERN'], $file) === 1) $problems[] = $file;
+    if (preg_match($_ENV['FILE_REGEX_PATTERN'], $file) === 1) {
+        $problems[] = $file;
+    }
 }
 
 if (!empty($problems)) {
-    preg_match($_ENV['PROBLEM_NUMBER_REGEX_PATTERN'], $problems[0], $number);
+    $filename = $problems[0];
+    preg_match($_ENV['PROBLEM_NUMBER_REGEX_PATTERN'], $filename, $number);
+    $number = $number[0];
 
-    $problem = $hunter->problem($number[0], "num");
-    $title = $problem["title"];
-    $id = $problem["id"];
+    $problem = $hunter->problem($number, 'num');
+    $title = $problem['title'];
+    $id = $problem['id'];
 
-    $message = $title != NULL? $_ENV['COMMIT_WITH_URL'] ? messageFormatter($number[0], $title, $id): messageFormatter($number[0], $title): $problems[0];
+    if ($title !== null) {
+        $message = commitMessageFormatter($number, $title, $id);
+    } else { // The file problem number does not correspond to any UVa problem number. We'll use the filename as the commit message.
+        $message = $filename;
+    }
 
-    exec("git -C ".$_ENV['REPOSITORY_DIR']. " pull origin master");
-    exec("mv ".$_ENV['PROBLEMS_DIR'].$problems[0]." ".$_ENV['REPOSITORY_DIR']);
-    exec("git -C ".$_ENV['REPOSITORY_DIR']. " add ".$problems[0]);
-    exec("git -C ".$_ENV['REPOSITORY_DIR']. " commit -m "."\"$message\"");
-    exec("git -C ".$_ENV['REPOSITORY_DIR']. " push origin master");
+    // Perform the commit.
+    exec("git -C $_ENV[REPOSITORY_DIR] pull origin master");
+    exec("mv $_ENV[PROBLEMS_DIR]$filename $_ENV[REPOSITORY_DIR]");
+    exec("git -C $_ENV[REPOSITORY_DIR] add $filename");
+    exec("git -C $_ENV[REPOSITORY_DIR] commit -m \"$message\"");
+    exec("git -C $_ENV[REPOSITORY_DIR] push origin master");
 } else if ($_ENV['NOTIFICATIONS']) {
     mail($_ENV['EMAIL'], $_ENV['SUBJECT'], $_ENV['MESSAGE']);
 }
 
+function commitMessageFormatter($number, $title, $id = null)
+{
+    $commit = sprintf($_ENV['COMMIT_TITLE_PATTERN'], $number, $title);
+    if ($_ENV['COMMIT_WITH_URL'] && $id !== null) {
+        $commit .= sprintf("\nhttps://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=%d", $id);
+    }
+
+    return $commit;
+}
+
 function loadConfig()
 {
-    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv = new Dotenv\Dotenv('../');
     $dotenv->load();
     $dotenv->required([
         'REPOSITORY_DIR',
         'PROBLEMS_DIR',
         'FILE_REGEX_PATTERN',
         'PROBLEM_NUMBER_REGEX_PATTERN',
-        'COMMIT_MESSAGE_PATTERN',
-        'COMMIT_COMMENT_PATTERN'
+        'COMMIT_TITLE_PATTERN'
     ]);
 
     $dotenv->required(['NOTIFICATIONS', 'COMMIT_WITH_URL'])
