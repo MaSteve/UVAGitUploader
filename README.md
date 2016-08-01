c04df077f84099f2658cf0ee23cb528d63933544
david-perez <hallaplay835@gmail.com>
2016-08-01 14:56:39 +0200

    Switches configuration to ENV variables - Adds composer.json and composer.lock

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ UVAGitUploader is a PHP script used for committing the UVA problems solved by a 
 ##Installation
 - Clone this repository in your computer: `git clone https://github.com/MaSteve/UVAGitUploader.git`
 - Run the composer installer: `composer install`
-- Modify the `src/uploader.php` file including your UVA problems repository path and your problems folder path.
+- Modify `.env.example` to include your UVA problems local repository path and your problems folder path.
 - If you want to receive email notifications each time the script can't commit any problem, add your email and set `NOTIFICATIONS` like `true`. (**Warnign**: email notifications need an email server configuration).
+- Rename `.env.example` to `.env`.
 - Add a cron job for automatic committing.
--- src/uploader.php
+++ src/uploader.php
@@ -1,25 +1,14 @@
 <?php
-use Hunter\Hunter;
 
 require __DIR__."/../vendor/autoload.php";
 
-define(REPOSITORY_DIR, "");
-define(PROBLEMS_DIR, ""); // We need two different folders.
-define(FILE_REGEX_PATTERN, "/UVA[0-9]{5}.cpp/");
-define(PROBLEM_NUMBER_REGEX_PATTERN, "/[0-9]{5}/");
+use Hunter\Hunter;
 
-define(NOTIFICATIONS, false);
-define(EMAIL, "");
-define(SUBJECT, "Empty problems folder");
-define(MESSAGE, "You have a problem LOL");
-
-define(COMMIT_MESSAGE_PATTERN, "%d %s");
-define(COMMIT_WITH_URL, true);
-define(COMMIT_COMMENT_PATTERN, "\n\nhttps://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=%d");
+loadConfig();
 
 function messageFormatter($number, $title, $id = NULL) {
-    $commit = sprintf(COMMIT_MESSAGE_PATTERN, $number, $title);
-    if ($id != NULL) $commit .= sprintf(COMMIT_COMMENT_PATTERN, $id);
+    $commit = sprintf($_ENV['COMMIT_MESSAGE_PATTERN'], $number, $title);
+    if ($id != NULL) $commit .= sprintf($_ENV['COMMIT_COMMENT_PATTERN'], $id);
     return $commit;
 }
 
@@ -27,28 +16,56 @@ $hunter = new Hunter();
 
 $ls = array();
 
-exec("ls ".PROBLEMS_DIR, $ls);
+exec("ls ".$_ENV['PROBLEMS_DIR'], $ls);
 
 $problems = array();
 
 foreach ($ls as $file) {
-    if (preg_match(FILE_REGEX_PATTERN, $file) === 1) $problems[] = $file;
+    if (preg_match($_ENV['FILE_REGEX_PATTERN'], $file) === 1) $problems[] = $file;
 }
 
 if (!empty($problems)) {
-    preg_match(PROBLEM_NUMBER_REGEX_PATTERN, $problems[0], $number);
+    preg_match($_ENV['PROBLEM_NUMBER_REGEX_PATTERN'], $problems[0], $number);
 
     $problem = $hunter->problem($number[0], "num");
     $title = $problem["title"];
     $id = $problem["id"];
 
-    $message = $title != NULL? COMMIT_WITH_URL? messageFormatter($number[0], $title, $id): messageFormatter($number[0], $title): $problems[0];
+    $message = $title != NULL? $_ENV['COMMIT_WITH_URL'] ? messageFormatter($number[0], $title, $id): messageFormatter($number[0], $title): $problems[0];
 
-    exec("git -C ".REPOSITORY_DIR. " pull origin master");
-    exec("mv ".PROBLEMS_DIR.$problems[0]." ".REPOSITORY_DIR);
-    exec("git -C ".REPOSITORY_DIR. " add ".$problems[0]);
-    exec("git -C ".REPOSITORY_DIR. " commit -m "."\"$message\"");
-    exec("git -C ".REPOSITORY_DIR. " push origin master");
-} else if (NOTIFICATIONS) {
-    mail(EMAIL, SUBJECT, MESSAGE);
+    exec("git -C ".$_ENV['REPOSITORY_DIR']. " pull origin master");
+    exec("mv ".$_ENV['PROBLEMS_DIR'].$problems[0]." ".$_ENV['REPOSITORY_DIR']);
+    exec("git -C ".$_ENV['REPOSITORY_DIR']. " add ".$problems[0]);
+    exec("git -C ".$_ENV['REPOSITORY_DIR']. " commit -m "."\"$message\"");
+    exec("git -C ".$_ENV['REPOSITORY_DIR']. " push origin master");
+} else if ($_ENV['NOTIFICATIONS']) {
+    mail($_ENV['EMAIL'], $_ENV['SUBJECT'], $_ENV['MESSAGE']);
+}
+
+function loadConfig()
+{
+    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
+    $dotenv->required([
+        'REPOSITORY_DIR',
+        'PROBLEMS_DIR',
+        'FILE_REGEX_PATTERN',
+        'PROBLEM_NUMBER_REGEX_PATTERN',
+        'COMMIT_MESSAGE_PATTERN',
+        'COMMIT_COMMENT_PATTERN'
+    ]);
+
+    $dotenv->required(['NOTIFICATIONS', 'COMMIT_WITH_URL'])
+        ->allowedValues(['true', 'false']);
+
+    $_ENV['NOTIFICATIONS'] = ($_ENV['NOTIFICATIONS'] === 'true');
+    $_ENV['COMMIT_WITH_URL'] = ($_ENV['COMMIT_WITH_URL'] === 'true');
+
+    if ($_ENV['NOTIFICATIONS']) {
+        $dotenv->required([
+            'EMAIL',
+            'SUBJECT',
+            'MESSAGE'
+        ]);
+    }
 }
